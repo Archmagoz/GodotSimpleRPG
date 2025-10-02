fe1836c244f489f9b411020e63881617dce8cc02
Archmagoz <0josegabrielmatos0@gmail.com>
2025-10-02 09:36:43 -0300

    backup

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -3,9 +3,13 @@ extends CharacterBody2D
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 @onready var speed: SpeedComponent = $SpeedComponent
 
+var is_attacking: bool = false
+
 func _physics_process(_delta: float) -> void:
-	_handle_movement()
-	
+	_handle_attack()
+	if not is_attacking:
+		_handle_movement()
+
 func _handle_movement() -> void:
 	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	
@@ -18,3 +22,10 @@ func _handle_movement() -> void:
 
 	velocity = direction * speed.get_speed()
 	move_and_slide()
+
+func _handle_attack() -> void:
+	if Input.is_action_pressed("attack"):
+		is_attacking = true
+		animation.play("attack")
+	else:
+		is_attacking = false
